12fd1bcf941a784fb65bb82dade883f69a6a6681
Lionel Faleiro <lionelxp@gmail.com>
2023-02-26 23:53:44 +0530

    Add files via upload

---
+++ BrowserForensics.ps1
@@ -0,0 +1,27 @@
+﻿Write-Host "Browser Forensics Capture"
+Write-Host "Create by Lionel Faleiro"
+
+$browserName = Read-Host "Enter the browser name (chrome/edge):"
+
+if ($browserName -eq "chrome") {
+    $browserPath = "Google\Chrome"
+} else {
+    $browserPath = "Microsoft\Edge"
+}
+
+
+
+$url = "https://github.com/obsidianforensics/hindsight/releases/download/v2021.12/hindsight.exe"
+$output = "$env:TEMP\hindsight.exe"
+$hindsightOp = "$env:TEMP\browserdump"
+$browserProfile = "$env:LOCALAPPDATA\$browserPath\User Data\Default"
+$hindsightParameter = "-i `"$browserProfile`" -o `"$hindsightOp $browserName`" -f xlsx"
+$finalexec = "$output $hindsightParameter"
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+Write-Host $output $hindsightParameter
+Start-Process $output $hindsightParameter -NoNewWindow
+
+
